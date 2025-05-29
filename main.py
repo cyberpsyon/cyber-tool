8f4d9e7238dca910c04621c8f5a4d9fbb9c1ed4e
bjmickens <128921000+bjmickens@users.noreply.github.com>
2025-05-28 22:42:55 -0500

    Added menu

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+import sys
+import pyfiglet
+
+def display_menu():
+    menu = """
+    [1] Subdomain Enumeration
+    [2] Directory Enumeration
+    [3] Network Scanner
+    [4] Port Scanner
+    [5] Hash Cracker
+    [6] SSH Brute Force
+    [7] Web Login Form Brute Force
+    [8] SQL Injection Exploit
+    [9] Exit
+    """
+    print(menu)
+
+def main():
+    print(pyfiglet.figlet_format("Cyber Tool", "slant"))
+    display_menu()
+    choice = int(input("Enter your choice: "))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
